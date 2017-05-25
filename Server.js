31ab2c1afac8a4409c310c59af4552bbde12b33d
Nick Monteleone <nicholas.monteleone@uconn.edu>
2017-05-25 13:10:57 -0400

    Got rid of node_modules and made a package.json file. Updated README file. Changed port to 80. Documented Server.js and got rid of some unnecessary lines of code.

--- Server.js
+++ Server.js
@@ -1,33 +1,34 @@
+//import necessary dependancies
 var express = require('express');
-var exec = require("child_process").exec;
 var execSync = require("child_process").execSync;
-var fs = require('fs');
-var path = require('path');
 var app = express();
-var jsonfile = require("jsonfile")
-var multer = require('multer');
 var bodyParser = require('body-parser');
-var stringDecoder = require("string_decoder").StringDecoder
-var sha256 = require("js-sha256")
+
+//set piAddress to empty string, set portNumber to 80, set count to 1
 var piAddress = ""
-//var upload = multer();
+var portNumber = 80
 var count = 1;
 
+//set up app in current directory
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
 
+//get the count. This is how many times the page had to refresh when searching for Raspberry Pi
 app.get("/count", function(req, res){
 	res.send(count.toString())
 })
 
+//increments the count variable
 app.get("/increment", function(req, res){
 	increment()
 })
 
+//resets the count variable
 app.get("/resetCount", function(req, res){
 	resetCount()
 })
 
+//changes the address to a new address given that the request is valid
 app.post("/changeAddress", function(req, res){
 	newAddress = req.body.address
 	if(newAddress){
@@ -39,18 +40,22 @@ app.post("/changeAddress", function(req, res){
 	}
 })
 
+//gets the piAddress
 app.get("/piAddress", function(req, res){
 	res.send(piAddress)
 })
 
+//increments the count
 function increment(){
 	count++
 }
 
+//resets the count
 function resetCount(){
 	count = 1
 }
 
+//queries a pokemon
 app.get("/pokemon", function(req, res){
 	pokeName = req.query.pokemon
 	pokeName = pokeName.replace("+", " ")
@@ -59,15 +64,10 @@ app.get("/pokemon", function(req, res){
 	res.send(result)
 })
 
-var server = app.listen(9876, function () {
-
-  var host = server.address().address;
+//sets up server on localhost with port corresponding to portNumber
+var server = app.listen(portNumber, function () {
   var port = server.address().port;
-
-  if(host == "::")
-  	host = "localhost"
-
-  console.log('Media Server running at http://%s:%s', host, port);
+  console.log('Media Server running at http://localhost:%s', port);
 
 });
 
