20abb5e68d2fdf8977c7ac8153e142a7e643f457
Nick Monteleone <nickjm6@gmail.com>
2017-11-24 16:30:56 -0500

    started to implement google Oauth and Passport

--- Server.js
+++ Server.js
@@ -6,13 +6,29 @@ var bodyParser = require('body-parser');
 var http = require("http");
 var ping = require("ping");
 var querystring = require("querystring");
+var mongoose = require("mongoose")
+var cookieParser = require("cookie-parser")
+var session = require("express-session")
+var passport = require("passport")
+
+var User = require("./config/user.js")
+var config = require("./config/config.js")
 
 //set piAddress to empty string, set portNumber to 80, set count to 1
 var piAddress;
 var portNumber = 80
-var count = 1;
 var addresses = []
 var sig = "MyRazPi";
+var mongoDB = "mongodb://localhost/test"
+
+mongoose.connect(mongoDB, {
+	useMongoClient: true
+});
+var db = mongoose.connection
+
+db.on("error", console.error.bind(console, "Could not connect to DB"));
+
+require("./config/passport")(passport)
 
 for(var i = 2; i < 256; i++){
 	addresses.push("192.168.0." + i);
@@ -115,8 +131,27 @@ var getAddr = function(){
 	});
 }
 
+var createUser = function(username, password){
+	var newUser = new User({
+		username: username,
+		password: password
+	});
+	newUser.save(function(err){
+		if(err){
+			console.log(err);
+		} else{
+			console.log("save successful");
+		}
+	})
+}
 //set up app in current directory
 app.use(express.static(__dirname));
+app.use(cookieParser());
+app.use(session({
+	secret: sig
+}));
+app.use(passport.initialize());
+app.use(passport.session())
 app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
 
 //gets the piAddress
@@ -142,6 +177,26 @@ app.get("/getVol", function(req, res){
 	}).catch(function(e){
 		res.status(e.status).send(e.err);
 	})
+})
+
+app.get("/", function(req, res){
+	res.sendFile(__dirname + "/html/index.html");
+})
+
+app.get("/login", function(req, res){
+	res.sendFile(__dirname + "/html/login.html");
+})
+
+app.get("/piController", function(req, res){
+	if(req.isAuthenticated()){
+		res.sendFile(__dirname + "/html/piController.html")
+	} else{
+		res.redirect("/login")
+	}
+})
+
+app.get("/pokemon", function(res, res){
+	res.sendFile(__dirname + "/html/pokemon.html")
 })
 
 app.post("/reboot", function(req, res){
@@ -194,6 +249,7 @@ app.post("/volumeDown", function(req, res){
 	})
 })
 
+
 //queries a pokemon
 app.get("/pokemon", function(req, res){
 	pokeName = req.query.pokemon
@@ -202,6 +258,27 @@ app.get("/pokemon", function(req, res){
 	result = execSync(command)
 	res.send(result)
 })
+
+app.get("/logout", function(req, res){
+	req.logout();
+	res.redirect("/")
+})
+
+app.get("/auth/google", passport.authenticate("google", {
+	scope: ['profile', 'email']
+}));
+
+app.get("/auth/google/callback",
+	passport.authenticate("google", {
+		successRedirect: "/piController",
+		failuteRedirect: "/"
+}))
+
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated())
+		return next();
+	res.status(403).send("not logged in")
+}
 
 // gets address before starting the server
 getAddr().then(function(){
---
+++ config/passport.js
@@ -0,0 +1,48 @@
+var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+var User = require("./user")
+var config = require("./config")
+
+module.exports = function(passport){
+	passport.serializeUser(function(user, done){
+		done(null, user.id)
+	});
+
+	passport.deserializeUser(function(id, done){
+		User.findById(id, function(err, user){
+			done(err, user);
+		})
+	})
+
+	passport.use(new GoogleStrategy({
+		clientID: config.clientID,
+		clientSecret: config.clientSecret,
+		callbackURL: config.callbackURL
+	},
+	function(token, refreshToken, profile, done){
+		process.nextTick(function(){
+			User.findOne({"google.id" : profile.id}, function(err, user){
+				if(err)
+					return done(err);
+
+				if(user){
+					return done(null, user);
+				} else{
+					// var newUser = new User();
+					// newUser.google.id = profile.id;
+					// newUser.google.token = token;
+					// newUser.google.name = profile.displayName;
+					// newUser.google.email = profile.emails[0].value;
+
+					// newUser.save(function(err){
+					// 	if(err)
+					// 		throw err;
+					// 	return done(null, newUser);
+					// })
+					return done("Not a valid User")
+				}
+
+			})
+		})
+	}))
+}
