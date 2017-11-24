02c9565d0f77bfc608290f9c7d085080fc24e1a4
Nick Monteleone <nickjm6@gmail.com>
2017-11-24 16:43:45 -0500

    change redirect to ip of server

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	'clientID'      : '402213851363-89hqolr403fa0lpqbu1753de9tgtjd23.apps.googleusercontent.com',
     'clientSecret'  : '-X45TW_VfoiF1Dc5ATMq3NRM',
-    'callbackURL'   : 'http://localhost/auth/google/callback'
+    'callbackURL'   : 'http://192.168.0.15/auth/google/callback'
 }
